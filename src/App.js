b7b64a7306fd5de3cbb3922b4bb591c9aacc6a10
peeweemiwie <miwayagi@gmail.com>
2021-11-12 08:17:06 -0500

    Fixed Form. Add header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Form from './components/Form';
 function App() {
 	return (
 		<div className='App'>
+			<header>
+				<h1>Weather App React 2.0</h1>
+			</header>
 			<Form />
 		</div>
 	);
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Form = () => {
-	<div className='Form'>
-		<form action=''>
-			<input type='text' name='' id='' />
-			<button type='submit'>Submit</button>
-		</form>
-	</div>;
+	return (
+		<div className='Form'>
+			<form action=''>
+				<input type='text' name='' id='' />
+				<button type='submit'>Submit</button>
+			</form>
+		</div>
+	);
 };
 
 export default Form;
