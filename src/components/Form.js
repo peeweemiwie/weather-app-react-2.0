37eac78158276131544dad92a7e990186d4f69c7
peeweemiwie <miwayagi@gmail.com>
2021-11-12 08:09:36 -0500

    Initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form = () => {
+	<div className='Form'>
+		<form action=''>
+			<input type='text' name='' id='' />
+			<button type='submit'>Submit</button>
+		</form>
+	</div>;
+};
+
+export default Form;
